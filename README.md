bc6671dd90c07972ee85b0545226d4bace1bb207
shrutisohi04 <shrutisohi04@gmail.com>
2023-08-06 15:12:41 +0530

    BASE SETUP

---
+++ README.md
@@ -0,0 +1,40 @@
+# CREATING DATA INPUT FORM
+
+This repository contains a simple web form that allows users to input data for a specific purpose. The form is designed to be user-friendly and responsive, adjusting its layout for both computer screens and mobile devices.
+
+## Features
+
+- User-friendly and interactive web form
+- Responsive design for different screen sizes
+- Pale blue background with dark blue text for better readability
+- Click effect on the submit button
+- Success page to display "FORM SUBMIT SUCCESSFULLY" on successful form submission
+
+## Preview
+
+
+## Usage
+
+1. Clone the repository to your local machine
+
+2. Open the `form.html` file in your web browser to access the interactive form.
+
+3. Fill in the form fields with the required data.
+
+4. Click the "Submit" button to submit the form.
+
+5. Upon successful submission, you will be redirected to a success page showing "FORM SUBMIT SUCCESSFULLY."
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Contributing
+
+Contributions to this project are welcome. If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+
+
+
+
---
+++ script.js
@@ -0,0 +1,40 @@
+// Function to handle form submission
+function handleSubmit(event) {
+    event.preventDefault();
+
+    // we can add more input fields and validation here if needed.
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    // Performing data validation here. That is checking if the email is valid.
+    if (!isValidEmail(emailInput)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    const contact = document.getElementById('phonenumber').value;
+    // Validate contact number
+    if (!isValidContactNumber(contact)) {
+        alert('Please enter a valid contact number.');
+        return;
+    }
+
+    // Redirect to the success page after form submission
+    window.location.href = 'success.html';
+}
+
+// Function to validate email using regular expression
+function isValidEmail(email) {
+    // Regular expression for email validation
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+// Function to validate contact number using regular expression
+function isValidContactNumber(contactNumber) {
+    // Regular expression for contact number validation
+    const contactNumberPattern = /^\d{10,}$/;
+    return contactNumberPattern.test(contactNumber);
+}
+
+// Event listener for form submission
+document.getElementById('userForm').addEventListener('submit', handleSubmit);
