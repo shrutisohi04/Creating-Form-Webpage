abff78a29d010f8fedf936aafc0e02952ae6764a
shrutisohi04 <shrutisohi04@gmail.com>
2023-08-07 13:43:32 +0530

    Modified

--- script.js
+++ script.js
@@ -5,22 +5,33 @@ function handleSubmit(event) {
     // we can add more input fields and validation here if needed.
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
+
     // Performing data validation here. That is checking if the email is valid.
-    if (!isValidEmail(emailInput)) {
+    if (!isValidEmail(email)) {
         alert('Please enter a valid email address.');
         return;
     }
 
     const contact = document.getElementById('phonenumber').value;
+
     // Validate contact number
     if (!isValidContactNumber(contact)) {
         alert('Please enter a valid contact number.');
         return;
     }
 
+    // Submit the form (you can optionally remove this line if you want to just redirect)
+    document.getElementById('userForm').submit();
+
     // Redirect to the success page after form submission
     window.location.href = 'success.html';
 }
+
+// Rest of the code remains the same...
+
+// Event listener for form submission
+document.getElementById('userForm').addEventListener('submit', handleSubmit);
+
 
 // Function to validate email using regular expression
 function isValidEmail(email) {
